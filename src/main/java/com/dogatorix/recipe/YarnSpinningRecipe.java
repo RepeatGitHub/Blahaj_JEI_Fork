51202b6fc477aa1a98f721f95d93b2713be1c899
Repeat <48736123+RepeatGitHub@users.noreply.github.com>
2024-09-20 18:08:10 -0400

    Update YarnSpinningRecipe.java

--- src/main/java/com/dogatorix/recipe/YarnSpinningRecipe.java
+++ src/main/java/com/dogatorix/recipe/YarnSpinningRecipe.java
@@ -1,15 +1,73 @@
 package com.dogatorix.recipe;
 
-import java.util.function.Supplier;
-
-import javax.annotation.ParametersAreNonnullByDefault;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-@ParametersAreNonnullByDefault
-public class YarnSpinningRecipe extends ProcessingRecipe<YarnSpinningRecipe.YarnSpinningWrapper> {
-    public static class YarnSpinningWrapper extends RecipeWrapper {
-  		public YarnSpinningWrapper() {
-  			super(new ItemStackHandler(1));
-  		}
-  	}
+public class YarnSpinningRecipe  implements Recipe<SimpleContainer> {
+    private final NonNullList<Ingredient> inputItems;
+    private final ItemStack output;
+    private final ResourceLocation id;
+
+    public GemPolishingRecipe(NonNullList<Ingredient> inputItems, ItemStack output, ResourceLocation id) {
+        this.inputItems = inputItems;
+        this.output = output;
+        this.id = id;
+    }
+
+    @Override
+    public boolean matches(SimpleContainer pContainer, Level pLevel) {
+        if(pLevel.isClientSide()) {
+            return false;
+        }
+
+        return inputItems.get(0).test(pContainer.getItem(0));
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer pContainer, RegistryAccess pRegistryAccess) {
+        return output.copy();
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int pWidth, int pHeight) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess pRegistryAccess) {
+        return output.copy();
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static class Type implements RecipeType<YarnSpinningRecipe> {
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "yarn_spinning";
+    }
+
+	
 }
