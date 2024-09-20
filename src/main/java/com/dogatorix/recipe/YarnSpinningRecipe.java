38dbe8657042d7e4d6391fb1c22c9977b6957873
Repeat <48736123+RepeatGitHub@users.noreply.github.com>
2024-09-20 17:55:57 -0400

    Create YarnSpinningRecipe.java

---
+++ src/main/java/com/dogatorix/recipe/YarnSpinningRecipe.java
@@ -0,0 +1,13 @@
+package com.dogatorix.recipe;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import net.minecraft.world.level.Level;
+
+@ParametersAreNonnullByDefault
+public class YarnSpinningRecipe extends ProcessingRecipe<YarnSpinningRecipe.YarnSpinningWrapper> {
+    public static class YarnSpinningWrapper extends RecipeWrapper {
+  		public YarnSpinningWrapper() {
+  			super(new ItemStackHandler(1));
+  		}
+  	}
+}
